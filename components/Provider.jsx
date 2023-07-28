fd02fd4a278a2b3a79165b2a55fbde878276f2e7
sketchbook21 <gilbert.hsu2@gmail.com>
2023-07-27 20:00:32 -0400

    first commit

---
+++ components/Provider.jsx
@@ -0,0 +1,9 @@
+"use client";
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+
+const Provider = ({ children, session }) => {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
+};
+
+export default Provider;
